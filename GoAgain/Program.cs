b56498e23bb405cd4a7a62b0adf18a0b8128b873
Jacob Garcia <jacobgarcia@rocketmortgage.com>
2022-08-02 13:41:22 -0400

    updates

---
+++ GoAgain/Program.cs
@@ -0,0 +1,37 @@
+﻿// instructions ask the user to enter a number print the square of the number, ask the user if they want to go again.
+// they can enter y or Y to go again, n or N to quit. 
+
+bool keepGoing = true;
+
+do
+{
+    Console.Write("Enter a number: ");
+    double number = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"That number squared is {number * number}");
+
+
+    // Ask the user if they want to go again 
+    bool valid = false;
+    do
+    {
+        Console.Write("Would you like to go again? (y/n)");
+        string entry = Console.ReadLine().ToUpper();
+
+        if (entry == "N" || entry == "NO")
+        {
+            valid = true;
+            keepGoing = false;
+        }
+        else if (entry == "Y" || entry == "YES")
+        {
+            valid = true;
+            keepGoing |= true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+        }
+
+    } while (valid == false);
+
+} while (keepGoing);
---
+++ LoopsDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿// For Loop 
+
+for (int i = 0; i < 10; i++) //fizz buzz 
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+for (int i = 0; i <= 10; i+=2)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+for (int i =21; i >= 0; i =i - 3)
+{
+    Console.WriteLine(i);
+}
+
+
+Console.WriteLine("All Done!");
+
+Console.WriteLine();
+// while loop 
+int w = 1;
+
+while (w < 5)
+{
+    Console.WriteLine("Inside while loop");
+    Console.WriteLine(w);
+    w++;
+}
+
+//example of a break statement inside a while
+Console.WriteLine();
+int w2 = 10;
+
+while (w2 >= 0)
+{
+    Console.WriteLine(w2);
+    w2--;
+
+    if (w2 == 6)
+    {
+        Console.WriteLine("Left loop because found 6");
+        break;
+    }
+}
+
+
+Console.WriteLine("onto do while loops");
+Console.WriteLine();
+
+// while loops condition is tested before each itteration, do while they are tested at the end 
+int w3 = 0;
+do
+{
+    w3++;
+    Console.WriteLine(w3);
+    
+} while (w3 < 10 );
+
+Console.WriteLine("All done with while loops, now for nested for loops");
+
+
